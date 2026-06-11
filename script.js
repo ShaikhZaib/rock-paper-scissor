14ef25bd2ab146608246ece2236a2facfa754728
Shaikh Zaib <shaikhzaib04@gmail.com>
2026-06-12 00:07:18 +0530

    complete step 3 of assignment

--- script.js
+++ script.js
@@ -3,4 +3,21 @@ function getComputerChoice(){
     return choice;
 }
 
+function getPlayerChoice(){
+    let choice = prompt("Enter your choice(1.Rock, 2.Paper, 3.Scissor): ");
+    if(choice == 1){
+        return 1;
+    } else if(choice == 2){
+        return 2;
+    } else if(choice == 3){
+        return 3;
+    } else{
+        console.log("Choose from given choices");
+    }
+}
+
 console.log(getComputerChoice());
+
+
+
+console.log(getPlayerChoice());
